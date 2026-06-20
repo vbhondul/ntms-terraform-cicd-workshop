21e1b3cc962f1b0d09ad693a6a72da3a3b0539c2
Vaibhav Bhondule <vaibhav.bhondule@gmail.com>
2026-06-20 11:31:42 +0000

    new subnet code changes

--- terraformfiles/main.tf
+++ terraformfiles/main.tf
@@ -10,9 +10,16 @@ resource "azurerm_virtual_network" "vnet" {
   address_space       = ["10.1.0.0/16"]
 }
 
-resource "azurerm_subnet" "subnet" {
+resource "azurerm_subnet" "web-snet" {
   name                 = "web-subnet"
   virtual_network_name = azurerm_virtual_network.vnet.name
   resource_group_name  = azurerm_resource_group.rg.name
   address_prefixes     = ["10.1.0.0/24"]
+}
+
+resource "azurerm_subnet" "app-snet" {
+  name                 = "app-subnet"
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  resource_group_name  = azurerm_resource_group.rg.name
+  address_prefixes     = ["10.1.1.0/24"]
 }
