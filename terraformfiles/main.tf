4821218478aea3ad3b9895578b579026980fb57e
Vaibhav Bhondule <vaibhav.bhondule@gmail.com>
2026-06-20 09:44:12 +0000

    network changed

--- terraformfiles/main.tf
+++ terraformfiles/main.tf
@@ -1,18 +1,18 @@
 resource "azurerm_resource_group" "rg" {
-  name     = "ayushi-rg-cicd-ntms-workshop"
+  name     = "vaibhav-rg-cicd-ntms-workshop"
   location = "East US"
 }
 
 resource "azurerm_virtual_network" "vnet" {
-  name                = "ayushi-vnet-cicd-workshop"
+  name                = "vaibhav-vnet-cicd-workshop"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  address_space       = ["10.0.0.0/16"]
+  address_space       = ["10.1.0.0/16"]
 }
 
 resource "azurerm_subnet" "subnet" {
   name                 = "web-subnet"
   virtual_network_name = azurerm_virtual_network.vnet.name
   resource_group_name  = azurerm_resource_group.rg.name
-  address_prefixes     = ["10.0.0.0/24"]
+  address_prefixes     = ["10.1.0.0/24"]
 }
