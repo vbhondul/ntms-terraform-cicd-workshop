03d859da9a4357676d6dfa9ce23c2f31ac359ab0
Vaibhav Bhondule <vaibhav.bhondule@gmail.com>
2026-06-20 11:43:34 +0000

    update providers

--- terraformfiles/provider.tf
+++ terraformfiles/provider.tf
@@ -5,6 +5,14 @@ terraform {
       version = "~> 4.0"
     }
   }
+
+
+  backend "azurerm" {
+    resource_group_name = "rg-terraform-state"
+    storage_account_name = "stntmstfstate"
+    container_name = "vaibhav" #replace with your name
+    key = "vaibhav.terraform.tfstate" #replace with your name
+  }
 }
 
 provider "azurerm" {
